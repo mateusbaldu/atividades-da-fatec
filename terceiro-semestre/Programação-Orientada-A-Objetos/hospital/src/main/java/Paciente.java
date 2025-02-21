9f5f7f3f23a9802001615681785cce5ae727d969
Mateus Balduino <balduinomateus0@gmail.com>
2025-02-20 21:19:15 -0300

    Adicionando os arquivos
    Adicionando os arquivos da atividade02 ao repostório

---
+++ terceiro-semestre/Programação-Orientada-A-Objetos/hospital/src/main/java/Paciente.java
@@ -0,0 +1,52 @@
+public class Paciente {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String genero;
+    private int idade;
+    private String senha;
+
+    public Paciente(){}
+
+    public Paciente(String nome, String cpf, String telefone, String genero, int idade, String senha) {
+        setNome(nome);
+        setCpf(cpf);
+        setTelefone(telefone);
+        setGenero(genero);
+        setIdade(idade);
+        setSenha(senha);
+    }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getTelefone() { return telefone; }
+
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getGenero() { return genero; }
+
+    public void setGenero(String genero) { this.genero = genero; }
+
+    public int getIdade() { return idade; }
+
+    public void setIdade(int idade) { this.idade = idade; }
+
+    public String getSenha() { return "*************"; }
+
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public void mostrar() {
+        System.out.println("\n\nnome: "+ getNome());
+        System.out.println("cpf: "+ getCpf());
+        System.out.println("senha: "+ getSenha());
+        System.out.println("genero: "+ getGenero());
+        System.out.println("telefone: "+ getTelefone());
+        System.out.println("idade: "+ getIdade());
+    }
+}
