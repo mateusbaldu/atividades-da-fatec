cf4b996d91b1a58cb06c2237d2a6141002d20986
Mateus Balduino <balduinomateus0@gmail.com>
2025-02-20 23:48:17 -0300

    Reorganização das pastas

---
+++ terceiro-semestre/Programação-Orientada-A-Objetos/atividade02/hospital/src/main/java/Medico.java
@@ -0,0 +1,65 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Medico {
+    private String nome;
+    private String crm;
+    private String telefone;
+    private String especialidade;
+    private String senha;
+
+    public Medico(String nome, String crm, String telefone, String especialidade, String senha) {
+        setNome(nome);
+        setCrm(crm);
+        setTelefone(telefone);
+        setEspecialidade(especialidade);
+        setSenha(senha);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getSenha() {
+        return "******************";
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void mostrar() {
+        System.out.println("\n\nnome: "+ getNome());
+        System.out.println("crm: "+ getCrm());
+        System.out.println("telefone: "+ getTelefone());
+        System.out.println("especialidade: "+ getEspecialidade());
+        System.out.println("senha: "+ getSenha());
+    }
+}
