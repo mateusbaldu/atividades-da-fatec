77ca8d45c805ca949bb82d2e7df79cad0e828be0
Mateus Balduino <balduinomateus0@gmail.com>
2025-03-06 16:36:55 -0300

    coding: atividade03
    inicio da atividade 03 de POO

---
+++ terceiro-semestre/Programação-Orientada-A-Objetos/atividade03/biblioteca/src/Leitor.java
@@ -0,0 +1,67 @@
+public class Leitor {
+    private int codigo;
+    private String nome;
+    private String email;
+    private double telefone;
+    private String documento;
+
+    public Leitor(int codigo, String nome, String email, double telefone, String documento) {
+        setCodigo(codigo);
+        setNome(nome);
+        setEmail(email);
+        setTelefone(telefone);
+        setDocumento(documento);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getTelefone() {
+        return telefone;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(double telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public void guardar() {}
+    public void carregar() {}
+    public void listar() {}
+
+    public void mostrar() {
+        System.out.println("\ncodigo: "+getCodigo());
+        System.out.println("nome: "+getNome());
+        System.out.println("email: "+getEmail());
+        System.out.println("telefone: "+getTelefone());
+        System.out.println("documento: "+getDocumento());
+    }
+}
--- terceiro-semestre/Programação-Orientada-A-Objetos/atividade03/biblioteca/src/Main.java
+++ terceiro-semestre/Programação-Orientada-A-Objetos/atividade03/biblioteca/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        CategoriaObra categoriaObra = new CategoriaObra(1234, "numerico");
+        categoriaObra.mostrar();
+
+        Obra obraUm = new Obra(333, "Mauricio de Souza","Turma da Monica","Panini",2012,"Turma da Monica", 2012557);
+        obraUm.mostrar();
+
+        Copia turmaDaMonica = new Copia(3335,"Reservada","Insira texto");
+        turmaDaMonica.mostrar();
+
+        Leitor Mateus = new Leitor(20240533,"Mateus Balduino","mateusficticio@email.com",912344321,"142.123.198-00");
+        Mateus.mostrar();
+
+        Reserva turmaDaMonicaUm = new Reserva(3335142,"21/02/2024","21/03/2024","Insira texto");
+        turmaDaMonicaUm.mostrar();
+
+        
     }
 }
